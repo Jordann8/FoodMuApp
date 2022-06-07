44df71a015923c9101ed1ec57cf6f95236f52bed
ahmaddzakki <ahmaddzakki9@gmail.com>
2022-06-07 11:11:50 +0700

    bindingin

--- app/src/main/java/com/example/foodmuapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodmuapp/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.foodmuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.foodmuapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
