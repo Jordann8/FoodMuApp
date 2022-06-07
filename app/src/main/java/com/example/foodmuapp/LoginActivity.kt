e78a372786579847696e659763ba00f3c964f16b
AjrielRM <ajrielrisqymaulana@gmail.com>
2022-06-07 11:20:20 +0700

    coba toast (roti bakar)

--- app/src/main/java/com/example/foodmuapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodmuapp/LoginActivity.kt
@@ -2,14 +2,29 @@ package com.example.foodmuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.foodmuapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener(){
+            if (binding.etUsername.text.isNullOrEmpty() && binding.etPassword.text.isNullOrEmpty()){
+                Toast.makeText(this, "Please fill out the required form", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
+
+
+
+
     }
 }
