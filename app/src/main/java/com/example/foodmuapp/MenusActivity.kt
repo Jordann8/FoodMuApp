9ec0d819b28b17308e3f623cb5aa728cb6095c22
Fathir <ahmadfathir0830@gmail.com>
2022-06-13 11:24:33 +0700

    menulogic

--- app/src/main/java/com/example/foodmuapp/MenusActivity.kt
+++ app/src/main/java/com/example/foodmuapp/MenusActivity.kt
@@ -2,12 +2,37 @@ package com.example.foodmuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodmuapp.home.MakananModel
+import com.example.foodmuapp.home.adapter.FoodItemAdapter
 
 class MenusActivity : AppCompatActivity() {
+    private lateinit var listMakanan: ArrayList<MakananModel>
+    private lateinit var newRecyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menus)
 
+        listMakanan.add(MakananModel("McFlurry", R.drawable.mcflurry, "Rp11.364"))
+        listMakanan.add(MakananModel("Beef Burger", R.drawable.beefburger, "Rp11.364"))
+        listMakanan.add(MakananModel("Big Mac", R.drawable.big_mac, "Rp22.364"))
+        listMakanan.add(MakananModel("Fish Burger", R.drawable.fish_burger, "Rp11.364"))
+        listMakanan.add(MakananModel("McChicken", R.drawable.mcchicken, "Rp11.364"))
+
+
+        newRecyclerView = findViewById(R.id.rv_menu)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+        listMakanan = arrayListOf()
+
+
+        newRecyclerView.adapter = FoodItemAdapter()
+
     }
 
-}
+    }
+
--- app/src/main/java/com/example/foodmuapp/home/MakananModel.kt
+++ app/src/main/java/com/example/foodmuapp/home/MakananModel.kt
@@ -3,4 +3,7 @@ package com.example.foodmuapp.home
 
 data class MakananModel (
     var title: String,
+    var image: Int,
+    var harga:String
+
         )
