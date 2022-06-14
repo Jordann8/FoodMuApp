7e5f04bc6fd153f3c48bbf3c4071539f7c74a5d0
ahmaddzakki <ahmaddzakki9@gmail.com>
2022-06-14 14:10:00 +0700

    Recycler View lagi

--- app/src/main/java/com/example/foodmuapp/MainFragment.kt
+++ app/src/main/java/com/example/foodmuapp/MainFragment.kt
@@ -7,14 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodmuapp.databinding.ActivityHomeBinding
 import com.example.foodmuapp.databinding.FragmentMainBinding
 import com.example.foodmuapp.databinding.FragmentProfileBinding
+import com.example.foodmuapp.home.MakananModel
+import com.example.foodmuapp.home.adapter.FoodItemAdapter
 
 class MainFragment : Fragment() {
     private  var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var listMakanan: ArrayList<MakananModel>
+    private lateinit var newRecyclerView: RecyclerView
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,15 +45,28 @@ class MainFragment : Fragment() {
         binding.cardViewMcd.setOnClickListener{
             val goToMCD = Intent(context, RestaurantActivity::class.java)
             startActivity(goToMCD)
+
+            listMakanan = arrayListOf()
+
+
+            listMakanan.add(MakananModel("McFlurry", gambar = R.drawable.mcflurry, "Rp11.500"))
+            listMakanan.add(MakananModel("Burger", gambar = R.drawable.beefburger, "Rp14.000"))
+            listMakanan.add(MakananModel("Big Mac", gambar = R.drawable.big_mac, "Rp34.500"))
+            listMakanan.add(MakananModel("PaNas 1", gambar = R.drawable.panas_1, "Rp45.500"))
+            listMakanan.add(MakananModel("PaNas 2", gambar = R.drawable.panas_2, "Rp46.500"))
+            listMakanan.add(MakananModel("Sprite", gambar = R.drawable.sprite, "Rp8.500"))
+
+
+            newRecyclerView = binding.rvMenu
+            newRecyclerView.layoutManager = LinearLayoutManager(context)
+            newRecyclerView.setHasFixedSize(true)
+
+
+            newRecyclerView.adapter = FoodItemAdapter()
+
         }
 
-
-
-
-
     }
-
-
 
 }
 
